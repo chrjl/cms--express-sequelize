a5cb018e51be5b853b516b9e88cd28d3f3e863f6
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-04 08:47:01 -0700

    build: bootstrap express server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger = require('morgan');
+const debug = require('debug')('app:server');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use('/public', express.static('public'));
+
+app.use((err, req, res, next) => {
+  const { status, message } = err;
+  res.status(status).json({ status, message });
+});
+
+app.listen(PORT, () => {
+  debug(`Listening on port ${PORT}`);
+});
