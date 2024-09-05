5b9b1b79ee5ba087bacd34afff9906ee3dc3f9e0
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-05 15:16:06 -0700

    feat(api/posts): keyword POST, DELETE routes

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -57,19 +57,48 @@ router
     res.sendStatus(204);
   });
 
-router.route('/:postId/keywords').get(async (req, res, next) => {
-  const post = await Post.findByPk(req.params.postId);
+router
+  .route('/:postId/keywords')
+  .get(async (req, res, next) => {
+    const post = await Post.findByPk(req.params.postId);
 
-  if (post === null) {
-    return next(createHttpError(404));
-  }
+    if (post === null) {
+      return next(createHttpError(404));
+    }
 
-  const keywords = await Keyword.findAll({
-    attributes: ['keyword'],
-    where: { postid: req.params.postId },
+    const keywords = await Keyword.findAll({
+      attributes: ['keyword'],
+      where: { postid: req.params.postId },
+    });
+
+    res.json(keywords.map(({ keyword }) => keyword));
+  })
+  .post(async (req, res, next) => {
+    const post = await Post.findByPk(req.params.postId);
+
+    if (post === null) {
+      return next(createHttpError(404));
+    }
+
+    const [keyword, created] = await Keyword.findOrCreate({
+      where: {
+        postId: req.params.postId,
+        keyword: req.body.keyword,
+      },
+    });
+
+    res.sendStatus(201);
   });
 
-  res.json(keywords.map(({ keyword }) => keyword));
+router.route('/:postId/keywords/:keyword').delete(async (req, res) => {
+  await Keyword.destroy({
+    where: {
+      postId: req.params.postId,
+      keyword: req.params.keyword,
+    },
+  });
+
+  res.sendStatus(204);
 });
 
 module.exports = router;
