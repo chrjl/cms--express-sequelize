d2363589bf6f60d375e0352c72935495e60be104
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-05 13:33:46 -0700

    feat(api/keywords): GET routes

--- src/routes/api/keywords.js
+++ src/routes/api/keywords.js
@@ -1,4 +1,29 @@
 const express = require('express');
+const sequelize = require('sequelize');
 const router = express.Router();
+
+const models = require('../../models');
+const { Keyword } = models;
+
+router.get('/', async (req, res) => {
+  const keywords = await Keyword.findAll({
+    attributes: [
+      [sequelize.fn('DISTINCT', sequelize.col('keyword')), 'keyword'],
+    ],
+  });
+
+  res.json(keywords.map(({ keyword }) => keyword));
+});
+
+router.get('/:keyword', async (req, res, next) => {
+  const postIds = await Keyword.findAll({
+    attributes: ['postId'],
+    where: {
+      keyword: req.params.keyword,
+    },
+  });
+
+  res.json(postIds.map(({ postId }) => postId));
+});
 
 module.exports = router;
