d735dc005bdb7815eae93010eece3863d83ebfec
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-05 13:33:21 -0700

    feat(api/posts): GET routes

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -1,5 +1,56 @@
 const express = require('express');
 const createHttpError = require('http-errors');
+const models = require('../../models');
+
+const { Post, Keyword } = models;
+Post.hasMany(Keyword);
+Keyword.belongsTo(Post, { foreignKey: 'postId' });
+
 const router = express.Router();
+
+router.route('/').get(async (req, res) => {
+  const posts = await Post.findAll({
+    include: [{ model: Keyword, attributes: ['keyword'] }],
+  });
+
+  res.json(
+    posts.map(
+      ({ id, title, description, body, createdAt, updatedAt, Keywords }) => ({
+        id,
+        title,
+        description,
+        body,
+        createdAt,
+        updatedAt,
+        keywords: Keywords.map(({ keyword }) => keyword),
+      })
+    )
+  );
+});
+
+router.route('/:postId').get(async (req, res, next) => {
+  const post = await Post.findByPk(req.params.postId);
+
+  if (post === null) {
+    return next(createHttpError(404));
+  }
+
+  res.json(post);
+});
+
+router.route('/:postId/keywords').get(async (req, res, next) => {
+  const post = await Post.findByPk(req.params.postId);
+
+  if (post === null) {
+    return next(createHttpError(404));
+  }
+
+  const keywords = await Keyword.findAll({
+    attributes: ['keyword'],
+    where: { postid: req.params.postId },
+  });
+
+  res.json(keywords.map(({ keyword }) => keyword));
+});
 
 module.exports = router;
