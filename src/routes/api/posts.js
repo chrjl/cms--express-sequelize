07a010e87d6f8a972b34cf7afe423c3282d96547
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-05 14:12:53 -0700

    feat(api/posts): POST, DELETE routes

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -8,35 +8,54 @@ Keyword.belongsTo(Post, { foreignKey: 'postId' });
 
 const router = express.Router();
 
-router.route('/').get(async (req, res) => {
-  const posts = await Post.findAll({
-    include: [{ model: Keyword, attributes: ['keyword'] }],
+router
+  .route('/')
+  .get(async (req, res) => {
+    const posts = await Post.findAll({
+      include: [{ model: Keyword, attributes: ['keyword'] }],
+    });
+
+    res.json(
+      posts.map(
+        ({ id, title, description, body, createdAt, updatedAt, Keywords }) => ({
+          id,
+          title,
+          description,
+          body,
+          createdAt,
+          updatedAt,
+          keywords: Keywords.map(({ keyword }) => keyword),
+        })
+      )
+    );
+  })
+  .post(async (req, res) => {
+    const post = await Post.create(req.body);
+    res.json({ id: post.id });
   });
 
-  res.json(
-    posts.map(
-      ({ id, title, description, body, createdAt, updatedAt, Keywords }) => ({
-        id,
-        title,
-        description,
-        body,
-        createdAt,
-        updatedAt,
-        keywords: Keywords.map(({ keyword }) => keyword),
-      })
-    )
-  );
-});
+router
+  .route('/:postId')
+  .get(async (req, res, next) => {
+    const post = await Post.findByPk(req.params.postId);
 
-router.route('/:postId').get(async (req, res, next) => {
-  const post = await Post.findByPk(req.params.postId);
+    if (post === null) {
+      return next(createHttpError(404));
+    }
 
-  if (post === null) {
-    return next(createHttpError(404));
-  }
+    res.json(post);
+  })
+  .delete(async (req, res) => {
+    await Post.destroy({
+      where: { id: req.params.postId },
+    });
 
-  res.json(post);
-});
+    await Keyword.destroy({
+      where: { postId: req.params.postId },
+    });
+
+    res.sendStatus(204);
+  });
 
 router.route('/:postId/keywords').get(async (req, res, next) => {
   const post = await Post.findByPk(req.params.postId);
--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(logger('dev'));
+app.use(express.json());
+
 app.use('/public', express.static('public'));
 app.use('/api-docs', routes.apiDocs);
 app.use('/api', routes.api);
